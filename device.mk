50f7bf4738c99bb02eef028fff207ca61e2a391c
Saurabh <saurabhchardereal@gmail.com>
2020-04-30 19:49:49 +0530

    X00T: Nuke powerhint.xml
    * currently not really useful for our platform
    
    Signed-off-by: Saurabh <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -399,10 +399,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
-# Public Libraries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Tri-state keys
 PRODUCT_PACKAGES += \
     KeyHandler
